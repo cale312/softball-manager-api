9124afc70f9bc84c83959e168616c4437a973e9c
Zikisa Cale <zikisacalesogaxa@gmail.com>
2017-10-07 15:18:08 +0200

    Added post route for new team

--- build/routes/teams.route.js
+++ build/routes/teams.route.js
@@ -14,22 +14,61 @@ class TeamsRouter {
             let code = res.statusCode;
             let msg = res.statusMessage;
             res.json({
-                teams: teams
+                code,
+                msg,
+                teams
             });
         })
             .catch((error) => {
             let code = res.statusCode;
             let msg = res.statusMessage;
             res.json({
+                code,
+                msg,
                 error
+            });
+        });
+    }
+    createTeam(req, res, next) {
+        const teamName = req.body.teamName;
+        const coach = req.body.coach;
+        const rank = req.body.rank;
+        const teamPlayers = req.body.teamPlayers;
+        if (!teamName || !coach || !rank) {
+            res.status(422).json({ message: 'All Fields Required.' });
+        }
+        const team = new teams_1.default({
+            teamName: teamName,
+            coach: coach,
+            rank: rank,
+            teamPlayers
+        });
+        team.save()
+            .then((team) => {
+            let code = res.statusCode;
+            let msg = res.statusMessage;
+            res.json({
+                code,
+                msg,
+                team
+            });
+        })
+            .catch((err) => {
+            let code = res.statusCode;
+            let msg = res.statusMessage;
+            res.json({
+                code,
+                msg,
+                err
             });
         });
     }
     routes() {
         this.router.get('/', this.getAllTeams);
+        this.router.post('/newteam', this.createTeam);
     }
 }
 exports.TeamsRouter = TeamsRouter;
 const teamsRoute = new TeamsRouter();
-// teamsRoute.routes();
+teamsRoute.routes();
 exports.default = teamsRoute.router;
--- routes/teams.route.ts
+++ routes/teams.route.ts
@@ -33,8 +33,49 @@ export class TeamsRouter {
       })
   }
 
+  public createTeam(req: Request, res: Response, next: NextFunction) {
+
+    const teamName: string = req.body.teamName;
+    const coach: string = req.body.coach;
+    const rank: number = req.body.rank;
+    const teamPlayers: any = req.body.teamPlayers;
+
+    if (!teamName || !coach || !rank) {
+      res.status(422).json({ message: 'All Fields Required.' });
+    }
+
+    const team = new Teams({
+      teamName: teamName,
+      coach: coach,
+      rank: rank,
+      teamPlayers
+    });
+
+    team.save()
+      .then( (team: any) => {
+        let code = res.statusCode;
+        let msg = res.statusMessage;
+        res.json({
+            code,
+            msg,
+            team
+        });
+      })
+      .catch( (err: any) => {
+        let code = res.statusCode;
+        let msg = res.statusMessage;
+        res.json({
+          code,
+          msg,
+          err
+        });
+      })
+
+  }
+
   routes() {
     this.router.get('/', this.getAllTeams);
+    this.router.post('/newteam', this.createTeam);
   }
 
 }
